540d14df4acd1dbcf7b12454c628f9c7275de995
Santiago <santiagojheffreyrojas1997@hotmail.com>
2022-09-28 15:59:44 -0500

    [success] terminate index.js file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-import app from './src/app.js'
+'use strict'
+
+const app = require('./src/app')
+const { sequelize } = require('./src/app/Models/index')
+
+app.listen(app.get('port'), app.get('host'), () => {
+  if (app.get('env') === 'development') {
+    console.log(`Server connected in http://${app.get('host')}:${app.get('port')}`)
+
+    sequelize.authenticate()
+      .then(() => console.log('Connected database'))
+      .catch(error => console.log('Error connecting database', error))
+  }
+})
