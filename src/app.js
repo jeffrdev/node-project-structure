7147af537cec036287b7adc74049a91040b02df9
Santiago <santiagojheffreyrojas1997@hotmail.com>
2022-09-29 19:20:48 -0500

    [success] configure server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-import express from 'express'
+'use strict'
+
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const routes = require('./routes/index')
+const app = express()
+
+app.set('host', process.env.NODE_HOST || '127.0.0.1')
+app.set('port', process.env.NODE_PORT || 4000)
+app.set('env', process.env.NODE_ENV || 'development')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+if(app.get('env') === 'development') app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(routes)
+
+module.exports = app
