11821720dd6567f2f41c14379c97a91687d3ca4c
Santiago <santiagojheffreyrojas1997@hotmail.com>
2022-10-03 12:20:18 -0500

    [success] improve the code of the src/app.js file

--- src/app.js
+++ src/app.js
@@ -10,16 +10,19 @@ const routes = require('./routes/index')
 
 const app = express()
 
+// Settings
 app.set('host', process.env.NODE_HOST || '127.0.0.1')
 app.set('port', process.env.NODE_PORT || 4000)
 app.set('env', process.env.NODE_ENV || 'development')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// Middleware
 if(app.get('env') === 'development') app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// Routes
 app.use(routes)
 
 module.exports = app
