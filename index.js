770dc1534c59a55ac309f7f1405a3dccca6b6cdd
Santiago <santiagojheffreyrojas1997@hotmail.com>
2022-10-18 14:48:35 -0500

    [success] improve the code

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 const app = require('./src/app')
 const { sequelize } = require('./src/app/Models/index')
 
-app.listen(app.get('port'), app.get('host'), () => {
-  if (app.get('env') === 'development') {
-    console.log(`Server connected in http://${app.get('host')}:${app.get('port')}`)
+app.listen(app.get('port'), app.get('host'), error => {
+  if (error) console.log(error)
 
-    sequelize.authenticate()
-      .then(() => console.log('Connected database'))
+  if (app.get('env') === 'development') {
+    console.log(`Online server in http://${app.get('host')}:${app.get('port')}`)
+
+    sequelize
+      .authenticate()
+      .then(() => console.log('Online database'))
       .catch(error => console.log('Error connecting database', error))
   }
 })
--- src/app.js
+++ src/app.js
@@ -6,24 +6,32 @@ const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
 
-const routes = require('./routes/index')
+const webRoutes = require('./routes/web/web')
+const apiRoutes = require('./routes/api/api')
 
 const app = express()
 
-// Settings
+/* SETTINGS */
+
 app.set('host', process.env.NODE_HOST || '127.0.0.1')
-app.set('port', process.env.NODE_PORT || 4000)
+app.set('port', process.env.NODE_PORT || 3000)
 app.set('env', process.env.NODE_ENV || 'development')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-// Middleware
-if(app.get('env') === 'development') app.use(morgan('dev'))
+/* MIDDLEWARE */
+
+if (app.get('env') === 'development') app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Routes
-app.use(routes)
+/* ROUTES */
+
+// WEB Routes
+app.use(webRoutes)
+
+// API Routes
+app.use('/api', apiRoutes)
 
 module.exports = app
