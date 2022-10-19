0498c660c228e17f731aca4ce8a399dd4670bd89
Santiago <santiagojheffreyrojas1997@hotmail.com>
2022-10-19 15:26:44 -0500

    [success] optimize routes

--- src/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-import apiRoutes from './api.js'
-
-const router = Router()
-
-router.use(apiRoutes)
-
-export default router
--- src/routes/web/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-import webRoutes from './web.js'
-
-const router = Router()
-
-router.use(webRoutes)
-
-export default router
